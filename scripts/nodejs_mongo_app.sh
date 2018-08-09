40f44b2d8e524f7748d9a90c2e0944063e63588e
Samm Zhu <14305018+SammZhu@users.noreply.github.com>
2018-08-09 11:12:46 +0800

    Update nodejs_mongo_app.sh

--- scripts/nodejs_mongo_app.sh
+++ scripts/nodejs_mongo_app.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+GUID=`hostname|awk -F. '{print $2}'`
+oc new-project smoke-test
+oc new-app nodejs-mongo-persistent
+watch oc get pod
+sleep 300
+oc get route
+curl -k -i --head https://nodejs-mongo-persistent-smoke-test.apps.${GUID}.mitzicom.com
+sleep 20
+oc delete project smoke-test
