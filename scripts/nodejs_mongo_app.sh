9f55949ecc2d7cef00b88fa75edb51498cd8502c
root <root@bastion.7e7f.example.opentlc.com>
2018-08-12 04:37:35 +0000

    correct

--- scripts/nodejs_mongo_app.sh
+++ scripts/nodejs_mongo_app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GUID=`hostname|awk -F. '{print $2}'`
-oc new-project smoke-test
+oc new-project smoke-test-nodejs
 oc new-app nodejs-mongo-persistent
 #watch oc get pod
 sleep 60
@@ -9,4 +9,5 @@ oc get pod
 oc get route
 curl -i --head http://nodejs-mongo-persistent-smoke-test.apps.${GUID}.example.opentlc.com
 sleep 20
-oc delete project smoke-test
+oc delete project smoke-test-nodejs
+oc project default
